13707450b0a467e29594cb65331a11b61697e6d9
Cynthia <CynthiaSpinner@gmail.com>
2024-03-28 16:17:13 -0500

    cleaned up instructions for CCR purposes

--- IterationStatements/Program.cs
+++ IterationStatements/Program.cs
@@ -6,20 +6,9 @@ namespace IterationStatements
     class Program
     {
         static void Main(string[] args)
-        // PLEASE READ
-        // ****had to remove "int numbers" from parameters to make it run...
-        // dont know why that was in there or how. kept throwing error on the name of the variable "numbers"
-        // "var 'numbers' = new List<int>();*****
-
-            //dispite; got it all working
-
-
-
+                       
         {
-            //TODO - Read each comment and complete its instruction
-            // like the example below
-
-            //Create a List called "numbers" - DONE!
+          
             var numbers = new List<int>();
 
             //Start------
